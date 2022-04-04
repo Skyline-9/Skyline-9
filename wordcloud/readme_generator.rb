6cd876bec925bbc1627d7e5eb3b34626ebda98d2
Skyline-9 <luo.richard@gmail.com>
2022-04-03 21:12:49 -0400

    :bug: Fix readme

--- wordcloud/readme_generator.rb
+++ wordcloud/readme_generator.rb
@@ -101,8 +101,6 @@ A new word cloud will be automatically generated when you [add your own word](ht
     markdown.concat("\n\n Check out the [previous word cloud](#{previous_cloud_url}) to see our community's **#{CloudTypes::CLOUDPROMPTS[-2]}**")
 
     markdown.concat("</div>")
-
-    markdown.concat("\n\n ### Need inspiration for your own README? Check out [How to Stand out on GitHub using Profile READMEs](https://medium.com/better-programming/how-to-stand-out-on-github-with-profile-readmes-dfd2102a3490?source=friends_link&sk=61df9c4b63b329ad95528b8d7c00061f)")
   end
 
   private
